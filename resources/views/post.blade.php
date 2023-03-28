3a6a6cbc021a288f3113e4d71c874830dd54bc92
rishisAryal <risharyal5@gmail.com>
2023-03-28 15:52:00 +0545

    second comit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+<body>
+<article>
+    <h1>
+        <a href="/post">My first Post</a>
+    </h1>
+    <p>    sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+    </p>
+</article>
+</body>
