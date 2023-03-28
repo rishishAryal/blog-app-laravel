53cff7c1093f16f44a2fcfb6adb996d3f218c7cb
rishisAryal <risharyal5@gmail.com>
2023-03-28 15:45:00 +0545

    first commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,61 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+<body>
+<article>
+    <h1>
+       <a href="/post">My first Post</a>
+    </h1>
+    <p>    sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+    </p>
+</article><article>
+    <h1>
+        <a href="/post">My second Post</a>
+    </h1>
+    <p>    sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+    </p>
+</article><article>
+    <h1>
+        <a href="/post">My third Post</a>
+    </h1>
+    <p>    sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+        sdfhdfjdsjfdsjkfjdsfjksdjkfsjdkfnjsdkfn
+    </p>
+</article>
+</body>
